86f923194e3e0a7b656e3c53531dd5287d97201c
EDITION INDUSTRIES 🇮🇳 <riyanshusinh@gmail.com>
2026-07-08 09:33:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==========================================
 // 🛠️ MAINTENANCE SWITCH CONTROLLER
 // ==========================================
-const MAINTENANCE_SWITCH = true; 
+const MAINTENANCE_SWITCH = false; 
 
 const GRID_SIZE = 8; 
 
